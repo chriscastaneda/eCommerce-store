84ef6968fda9eaf50e130a9807005f09ddfa4d99
chris castaneda <chris.j.castaneda@gmail.com>
2020-03-13 20:41:32 -0700

    Delete product

--- components/Product/ProductAttributes.js
+++ components/Product/ProductAttributes.js
@@ -1,17 +1,20 @@
+import React from "react";
 import { Header, Button, Modal } from "semantic-ui-react";
 import axios from "axios";
 import baseUrl from "../../utils/baseUrl";
+import { useRouter } from "next/router";
 
 /**components */
 function ProductAttributes({ description, _id }) {
-  /*trash button switch */
-  const [modal, setModal] = React.useState(false);
+  const [modal, setModal] = React.useState(false); /*trash button switch */
+  const router = useRouter(); /*redirect to home page after delete */
 
   /**Delete Function */
   async function handleDelete() {
     const url = `${baseUrl}/api/product`;
     const payload = { params: { _id } };
     await axios.delete(url, payload);
+    router.push("/");
   }
 
   return (
@@ -26,10 +29,10 @@ function ProductAttributes({ description, _id }) {
       />
 
       <Modal open={modal} dimmer="blurring" /**Deletet Menu */>
-        <Modal.Header> Confirm Delete</Modal.Header>
-        <Modal.content>
+        <Modal.Header>Confirm Delete</Modal.Header>
+        <Modal.Content>
           <p>Are you sure you want to delete this product?</p>
-        </Modal.content>
+        </Modal.Content>
         <Modal.Actions>
           <Button
             onClick={() => setModal(false)} /**Hide menu */
--- pages/api/product.js
+++ pages/api/product.js
@@ -1,8 +1,27 @@
 import Product from "../../models/Product";
 
-/**Find/Export schema object */
 export default async (req, res) => {
-  const { _id } = req.query; //create obj
-  const product = await Product.findOne({ _id: _id }); //findOne id, pass obj(_d) to product variable
-  res.status(200).json(product);
+  switch (req.method) {
+    case "GET":
+      await handleGetRquest(req, res);
+      break;
+    case "DELETE":
+      await handleDeleteRquest(req, res);
+      break;
+    default:
+      res.status(405).send(`Method ${req.method} not allowed`);
+      break;
+  }
 };
+
+async function handleGetRquest(req, res) {
+  const { _id } = req.query; //create obj
+  const product = await Product.findOne({ _id }); //findOne id, pass obj(_d) to product variable
+  res.status(200).json(product);
+}
+
+async function handleDeleteRquest(req, res) {
+  const { _id } = req.query;
+  await Product.findOneAndDelete({ _id });
+  res.status(204).json({});
+}
